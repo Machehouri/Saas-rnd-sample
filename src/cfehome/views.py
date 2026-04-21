d3c574d0c1fd0acd02962d3da0672da9cc18dc77
Hamza Machehouri <117505006+Machehouri@users.noreply.github.com>
2026-04-21 10:25:53 +0100

    Refactor URL routing and views; add about page and update home page logic

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,27 +6,28 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
-    qs = PageVisit.objects.all()
-    page_qs = PageVisit.objects.filter(path=request.path)
-    my_title = "My Home Page"
-    my_context = {
-        "page_title": my_title,
-        "page_visits_count": page_qs.count(),
-        "total_visits_count": qs.count(),
+def home_view(request, *args, **kwargs):
+    # qs = PageVisit.objects.all()
+    # page_qs = PageVisit.objects.filter(path=request.path)
+    # my_title = "My Home Page"
+    # my_context = {
+    #     "page_title": my_title,
+    #     "page_visits_count": page_qs.count(),
+    #     "total_visits_count": qs.count(),
         
-    }
-    path = request.path
-    print("path", path)
-    html_template = "home.html"
-    PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)
+    # }
+    # path = request.path
+    # print("path", path)
+    # html_template = "home.html"
+    # PageVisit.objects.create(path=request.path)
+    # return render(request, html_template, my_context)
+    return about_view(request, *args, **kwargs)
 
 def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     try:
-        percent = (page_qs.count() * 100.0) / qs.count(),
+        percent = (page_qs.count() * 100.0) / qs.count()
     except:
         percent = 0
     my_title = "My About Page"
