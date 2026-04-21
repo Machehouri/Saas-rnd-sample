83acd565568e806053bb1e721823c074c6d7e2e2
Hamza Machehouri <117505006+Machehouri@users.noreply.github.com>
2026-04-21 11:55:13 +0100

    Update requirements and settings to include python-decouple for environment variable management

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xxp$p&-*_0acq_zpkh)a_-eezr$i(2omokw)bbb4r=__2*c3)j'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_FEBUG")).lower() =="true"
-
+#DEBUG = str(os.environ.get("DJANGO_FEBUG")).lower() =="true"
+DEBUG = config("DJANGO_FEBUG")
 print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [".railway.app"
